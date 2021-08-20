53396a036afd52c54ecfd56ce53c1779ffd72249
iqbal <muhammad.mahendra@students.amikom.ac.id>
2021-08-20 20:53:26 +0700

    chart

--- lib/widget/transaction_list.dart
+++ lib/widget/transaction_list.dart
@@ -24,11 +24,12 @@ class TransactionList extends StatelessWidget {
                   height: 20,
                 ),
                 Container(
-                    height: 200,
-                    child: Image.asset(
-                      'assets/images/waiting2.png',
-                      fit: BoxFit.cover,
-                    )),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting2.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ],
             )
           : ListView.builder(
